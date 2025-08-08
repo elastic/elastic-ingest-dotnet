48c0b53a95679889691c3612122bc518e142f8e0
Jan Calanog <jan.calanog@elastic.co>
2025-08-08 15:55:26 +0200

    Simplify test

--- tests/Elastic.Ingest.Elasticsearch.IntegrationTests/SemanticIndexIngestionTests.cs
+++ tests/Elastic.Ingest.Elasticsearch.IntegrationTests/SemanticIndexIngestionTests.cs
@@ -9,7 +9,6 @@ using Elastic.Channels;
 using Elastic.Clients.Elasticsearch.IndexManagement;
 using Elastic.Ingest.Elasticsearch.Indices;
 using Elastic.Ingest.Elasticsearch.Semantic;
-using Elastic.Transport.Extensions;
 using FluentAssertions;
 using Xunit;
 using Xunit.Abstractions;
@@ -119,94 +118,6 @@ public class SemanticIndexIngestionTests(IngestionCluster cluster, ITestOutputHe
 		// Verify the custom analyzer is configured in the index settings
 		var indexSettings = index.Indices[indexName].Settings;
 		indexSettings.Should().NotBeNull();
-
-		// Check that the analysis settings contain our custom analyzer
-		var analysisSettings = indexSettings?.Analysis;
-		analysisSettings.Should().NotBeNull();
-		analysisSettings?.Analyzers.Should().ContainKey("semantic_analyzer");
-
-		var semanticAnalyzer = analysisSettings?.Analyzers["semantic_analyzer"];
-		semanticAnalyzer.Should().NotBeNull();
-		// Verify the analyzer configuration matches what we defined in GetMappingSetting
-		semanticAnalyzer?.Type.Should().Be("custom");
-		semanticAnalyzer?.Tokenizer.Should().Be("standard");
-		semanticAnalyzer?.Filter.Should().Contain("lowercase").And.Contain("asciifolding");
-
-		// Verify inference endpoints exist
-		var inferenceResponse = await Client.Transport.GetAsync<Elastic.Transport.VoidResponse>($"_inference/sparse_embedding/{channel.InferenceId}");
-		inferenceResponse.ApiCallDetails.HttpStatusCode.Should().Be(200, "Index inference endpoint should exist");
-
-		var searchInferenceResponse = await Client.Transport.GetAsync<Elastic.Transport.VoidResponse>($"_inference/sparse_embedding/{channel.SearchInferenceId}");
-		searchInferenceResponse.ApiCallDetails.HttpStatusCode.Should().Be(200, "Search inference endpoint should exist");
-	}
-
-	[Fact]
-	public async Task EnsureDocumentsEndUpInSemanticIndexWithPreexistingInference()
-	{
-		var indexPrefix = "semantic-preexisting-";
-		var slim = new CountdownEvent(1);
-
-		// First create the inference endpoints manually to simulate preexisting ones
-		var inferenceId = "preexisting-elser-inference";
-		var searchInferenceId = "preexisting-search-elser-inference";
-
-		// Create inference endpoints
-		await CreateElserInferenceAsync(inferenceId, 1);
-		await CreateElserInferenceAsync(searchInferenceId, 2);
-
-		var options = new SemanticIndexChannelOptions<CatalogDocument>(Client.Transport)
-		{
-			IndexFormat = indexPrefix + "{0:yyyy.MM.dd}",
-			BulkOperationIdLookup = c => c.Id,
-			TimestampLookup = c => c.Created,
-			InferenceId = inferenceId,
-			SearchInferenceId = searchInferenceId,
-			UsePreexistingInferenceIds = true,
-			BufferOptions = new BufferOptions
-			{
-				WaitHandle = slim, OutboundBufferMaxSize = 1,
-			}
-		};
-
-		var channel = new SemanticIndexChannel<CatalogDocument>(options);
-		var bootstrapped = await channel.BootstrapElasticsearchAsync(BootstrapMethod.Failure, "7-days-default");
-		bootstrapped.Should().BeTrue("Expected to be able to bootstrap semantic index channel with preexisting inference endpoints");
-
-		var date = DateTimeOffset.Now;
-		var indexName = string.Format(options.IndexFormat, date);
-
-		channel.TryWrite(new CatalogDocument { Created = date, Title = "Preexisting inference test", Id = "preexisting-test" });
-		if (!slim.WaitHandle.WaitOne(TimeSpan.FromSeconds(10)))
-			throw new Exception($"semantic document was not persisted within 10 seconds: {channel}");
-
-		var refreshResult = await Client.Indices.RefreshAsync(indexName);
-		refreshResult.IsValidResponse.Should().BeTrue("{0}", refreshResult.DebugInformation);
-		var searchResult = await Client.SearchAsync<CatalogDocument>(s => s.Indices(indexName));
-		searchResult.Total.Should().Be(1);
-
-		var storedDocument = searchResult.Documents.First();
-		storedDocument.Id.Should().Be("preexisting-test");
-		storedDocument.Title.Should().Be("Preexisting inference test");
-	}
-
-	private async Task CreateElserInferenceAsync(string inferenceId, int numThreads)
-	{
-		var data = $$"""
-			{
-			  "service": "elser",
-			  "service_settings": {
-			    "adaptive_allocations": {
-			      "enabled": true,
-			      "min_number_of_allocations": 3,
-			      "max_number_of_allocations": 10
-			    },
-			    "num_threads": {{numThreads:N0}}
-			  }
-			}
-			""";
-
-		await Client.Transport.PutAsync<Elastic.Transport.StringResponse>(
-			$"_inference/sparse_embedding/{inferenceId}",
-			Elastic.Transport.PostData.String(data));
+		indexSettings?.Analysis?.Analyzers.Should().ContainKey("semantic_analyzer");
 	}
 }
